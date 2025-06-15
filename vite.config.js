980af1c328dff1cc84e670cd11e0683296b9ec97
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-06-15 02:14:56 -0700

    Configure postcss-nesting for vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import postcssNesting from "postcss-nesting";
 import path from "path";
 
 export default defineConfig({
@@ -6,6 +7,11 @@ export default defineConfig({
     alias: {
       "@lib": path.resolve(__dirname, "./lib/index.ts"),
       "@components": path.resolve(__dirname, "./src/components/index.ts"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [postcssNesting()],
     },
   },
 });
