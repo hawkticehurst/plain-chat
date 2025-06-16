6f66bfeaf85af291ad9435e32d5b0a5d755abad4
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-06-16 08:19:25 -0700

    Create new Dialog component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { ChatMessage } from "./ChatMessage";
 export { ChatMessages } from "./ChatMessages";
 export { ChatSidebar } from "./ChatSidebar";
 export { UsageDashboard } from "./UsageDashboard";
+export { ConfirmDialog } from "./ConfirmDialog";
 export {
   NotificationComponent,
   notificationService,
